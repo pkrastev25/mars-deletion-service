966ba916fd5420a0c95a6c2388b4b398d99f0251
Petar <petar.krastev@haw-hamburg.de>
2018-05-25 18:59:26 +0300

    [FIX] IntegrationTests v3

--- IntegrationTests/_MountVolumes/mongodb/seed_data.sh
+++ IntegrationTests/_MountVolumes/mongodb/seed_data.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 # Recreate the DB using database-utility-svc:8090/storage/mongodb-meta
-mongorestore --host mongodb --port 27017 --archive=/data/mongodb.bson
+mongorestore --host mongodb --port 27017 --archive=/data/mongodb.bson --drop
--- IntegrationTests/_MountVolumes/result-mongodb/seed_data.sh
+++ IntegrationTests/_MountVolumes/result-mongodb/seed_data.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 # Recreate the DB using database-utility-svc:8090/storage/mongodb-result
-mongorestore --host result-mongodb --port 27017 --archive=/data/result-mongodb.bson
+mongorestore --host result-mongodb --port 27017 --archive=/data/result-mongodb.bson --drop
