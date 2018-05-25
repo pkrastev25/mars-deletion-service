a8049457eee31dcc2545c531953262dddd5e64f4
Petar <petar.krastev@haw-hamburg.de>
2018-05-25 20:15:59 +0300

    [FIX] Changed the db files once again

--- IntegrationTests/_MountVolumes/mongodb/seed_data.sh
+++ IntegrationTests/_MountVolumes/mongodb/seed_data.sh
@@ -1,16 +1,16 @@
 #!/usr/bin/env bash
 
 # file-svc
-mongorestore --gzip --host mongodb --port 27017 -db importFiles --archive=/data/importFiles.agz &&
+mongorestore --host mongodb --port 27017 -db importFiles --archive=/data/importFiles.archive &&
 
 # metadata -svc
-mongorestore --gzip --host mongodb --port 27017 -db import --archive=/data/import.agz &&
+mongorestore --host mongodb --port 27017 -db import --archive=/data/import.archive &&
 
 # scenario-svc
-mongorestore --gzip --host mongodb --port 27017 -db mars_websuite --archive=/data/mars_websuite.agz &&
+mongorestore --host mongodb --port 27017 -db mars_websuite --archive=/data/mars_websuite.archive &&
 
 # resultcfg-svc
-mongorestore --gzip --host mongodb --port 27017 -db Configs --archive=/data/Configs.agz &&
+mongorestore --host mongodb --port 27017 -db Configs --archive=/data/Configs.archive &&
 
 # sim-runner
-mongorestore --gzip --host mongodb --port 27017 -db mars-mission-control --archive=/data/mars-mission-control.agz
+mongorestore --host mongodb --port 27017 -db mars-mission-control --archive=/data/mars-mission-control.archive
