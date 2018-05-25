8a0ff8ed16beecfe154ad7a5124c541c39c5b79b
Petar <petar.krastev@haw-hamburg.de>
2018-05-25 19:30:59 +0300

    [FIX] Import of data using mongorestore for all dbs

--- IntegrationTests/_MountVolumes/mongodb/seed_data.sh
+++ IntegrationTests/_MountVolumes/mongodb/seed_data.sh
@@ -1,4 +1,16 @@
 #!/usr/bin/env bash
 
-# Recreate the DB using database-utility-svc:8090/storage/mongodb-meta
-mongorestore --host mongodb --port 27017 --archive=/data/mongodb.bson --drop
+# file-svc
+mongorestore --gzip --host mongodb --port 27017 -db importFiles --archive=/data/importFiles.agz &&
+
+# metadata -svc
+mongorestore --gzip --host mongodb --port 27017 -db import --archive=/data/import.agz &&
+
+# scenario-svc
+mongorestore --gzip --host mongodb --port 27017 -db mars_websuite --archive=/data/mars_websuite.agz &&
+
+# resultcfg-svc
+mongorestore --gzip --host mongodb --port 27017 -db Configs --archive=/data/Configs.agz &&
+
+# sim-runner
+mongorestore --gzip --host mongodb --port 27017 -db mars-mission-control --archive=/data/mars-mission-control.agz &&
