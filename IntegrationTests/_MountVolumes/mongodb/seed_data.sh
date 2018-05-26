7baa8b363b7924c992108e850b3898d66a1b4e61
Petar <petar.krastev@haw-hamburg.de>
2018-05-26 11:40:16 +0300

    [FIX] IntegrationTests v4

--- IntegrationTests/_MountVolumes/mongodb/seed_data.sh
+++ IntegrationTests/_MountVolumes/mongodb/seed_data.sh
@@ -1,16 +1,16 @@
 #!/usr/bin/env bash
 
 # file-svc
-mongorestore --drop --host mongodb --port 27017 -db importFiles --archive=/seed-data/importFiles.bson
+mongorestore --drop --host mongodb --port 27017 --db importFiles --archive=/seed-data/importFiles.bson &&
 
 # metadata -svc
-mongorestore --drop --host mongodb --port 27017 -db import --archive=/seed-data/import.bson
+mongorestore --drop --host mongodb --port 27017 --db import --archive=/seed-data/import.bson &&
 
 # scenario-svc
-mongorestore --drop --host mongodb --port 27017 -db mars_websuite --archive=/seed-data/mars_websuite.bson
+mongorestore --drop --host mongodb --port 27017 --db mars_websuite --archive=/seed-data/mars_websuite.bson &&
 
 # resultcfg-svc
-mongorestore --drop --host mongodb --port 27017 -db Configs --archive=/seed-data/Configs.bson
+mongorestore --drop --host mongodb --port 27017 --db Configs --archive=/seed-data/Configs.bson &&
 
 # sim-runner
-mongorestore --drop --host mongodb --port 27017 -db mars-mission-control --archive=/seed-data/mars-mission-control.bson
+mongorestore --drop --host mongodb --port 27017 --db mars-mission-control --archive=/seed-data/mars-mission-control.bson
--- IntegrationTests/_MountVolumes/result-mongodb/seed_data.sh
+++ IntegrationTests/_MountVolumes/result-mongodb/seed_data.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 # database-utility-svc
-mongorestore --drop --host result-mongodb --port 27017 -db ResultData --archive=/seed-data/ResultData.bson
+mongorestore --drop --host result-mongodb --port 27017 --db ResultData --archive=/seed-data/ResultData.bson
