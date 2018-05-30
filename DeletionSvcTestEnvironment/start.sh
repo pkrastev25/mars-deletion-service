30d0fbc0080f2706347b3ffc22e401b258aa5600
Petar <petar.krastev@haw-hamburg.de>
2018-05-30 14:14:04 +0300

    [FIX] Fix GitLab CI v3

--- DeletionSvcTestEnvironment/start.sh
+++ DeletionSvcTestEnvironment/start.sh
@@ -2,11 +2,12 @@
 # Used to build the docker container which contains all dependencies needed to run integration and end-to-end tests
 # Then it pushed it to the Nexus
 
+# HINT: In order to build the image from your local machine, use:
+# docker login -u HAW_USER_NAME ${GITLAB_REGISTRY}, where HAW_USER_NAME is your account!
+
 GITLAB_REGISTRY="docker-hub.informatik.haw-hamburg.de"
 PROJECT="mars/mars-deletion-svc"
-SERVICE_NAME="deletion-svc
+SERVICE_NAME="deletion-svc"
 
-# In order to push images from your local machine, use:
-#docker login -u HAW_USER_NAME -p HAW_PASSWORD
 docker build -t ${GITLAB_REGISTRY}/${PROJECT}/${SERVICE_NAME} .
 docker push ${GITLAB_REGISTRY}/${PROJECT}/${SERVICE_NAME}
